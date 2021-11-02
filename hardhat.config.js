05d15741d1a3245807c1ca48e717cd946dc23d4f
Parth Suthar <sparthcp@gmail.com>
2021-11-02 14:56:51 +0530

    Removed private key

--- hardhat.config.js
+++ hardhat.config.js
@@ -27,7 +27,7 @@ module.exports = {
 		},
 		ropsten: {
 			url: "https://ropsten.infura.io/v3/35ce16759f384d9a823fb9dd7457f9c0",
-			accounts:[`0xe82bb10c2bb9632598eb84c78d345a858f509c674fb0461fe6bee8574ea63668`] //[`0x${your - private - key}`],
+			accounts:[`0x${process.env.REACT_APP_MF_DEPLOYER_PRIVATE_ADDR}`] //[`0x${your - private - key}`],
 		},
 	},
 };
